715f4b40fb2078ddc15229ad3673c5b7942ef51c
RehberSingh <rehbersinghbasra@gmail.com>
2024-02-11 11:45:01 +0530

    gunicorn support

--- app.py
+++ app.py
@@ -133,6 +133,3 @@ def test():
 
         
 
-if __name__ == '__main__':
-    app.run(threaded=True, port=int(os.environ.get("PORT",
-                                                   os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
